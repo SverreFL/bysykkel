2036828e876adcad39260c370fce5cdfd90a0d21
Sverre Falck Langaas <svlan@fiskeridir.no>
2023-11-17 10:23:17 +0100

    bugfix: rettet opp i datasammenstilling

--- app.R
+++ app.R
@@ -8,19 +8,39 @@ library(bslib)
 
 stations <- read_csv("resources/data/stations.csv")
 trips <- read_csv("resources/data/trips.csv") %>%
-  rename("from" = `...1`,
+  rename(from = `...1`,
          to = `1`)
 
 paths <- st_read("resources/geometry/geometries_new.geojson")
 
-
-path_trips <- paths %>%
-  left_join(trips) %>%
-  left_join(stations, join_by(to == start_station_id)) %>%
-  filter(num_trips > 0) %>% 
+trips_paths <- trips %>% 
+  left_join(paths) %>% 
+  left_join(
+    stations %>% 
+      select(start_station_id,
+             start_name = name),
+    join_by(from == start_station_id)
+    ) %>% 
+  left_join(
+    stations %>% 
+      select(start_station_id,
+             end_name = name,
+             end_longitude = longitude,
+             end_latitude = latitude),
+    join_by(to == start_station_id)
+  ) %>% 
   mutate(num_trips_binned = cut(num_trips,
-                    breaks = quantile(num_trips, probs = seq(0, 1, 0.1)),
-                    labels = FALSE))
+                                breaks = quantile(num_trips, probs = seq(0, 1, 0.1)),
+                                labels = FALSE),
+         num_trips_binned = tidyr::replace_na(num_trips_binned, 1),
+         num_trips_binned = if_else(num_trips == 0, 0, num_trips_binned)
+         ) %>% 
+  sf::st_as_sf()
+  
+
+  
+
+
 
 green <- "#008a34"
 tiles_url <- "https://api.mapbox.com/styles/v1/svlan/clowzjqm6010u01o41ngl6vnm/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1Ijoic3ZsYW4iLCJhIjoiY2xvd3poem05MDEyMDJpcDgxamR4b2ZvbSJ9.LFIFyDd8pHDJl0RDksgOsA"
@@ -61,18 +81,17 @@ server <- function(input, output, session) {
   
   observe({
     akt_station_id = input$m_marker_click$id
-    akt_paths <- path_trips %>% 
-      filter(from == akt_station_id) %>% 
-      mutate(lab = glue("{name}<br>Antall turer: {num_trips}"))
-    print(akt_paths)
     
+    akt_paths <- trips_paths %>% 
+      filter(from == akt_station_id) %>% 
+      mutate(lab = glue("{end_name}<br>Antall turer: {num_trips}"))
+  
     leafletProxy("m") %>% 
       clearShapes() %>% 
-      clearPopups() %>% 
       addCircleMarkers(
         data = akt_paths,
-        lat = ~latitude,
-        lng = ~longitude,
+        lat = ~end_latitude,
+        lng = ~end_longitude,
         color = green,
         radius = 5,
         opacity = 1,
@@ -81,10 +100,11 @@ server <- function(input, output, session) {
         group = "stations",
         layerId = ~to
       ) %>% 
-      addPolylines(data=akt_paths,
+      addPolylines(data=akt_paths$geometry,
                    color = "black",
                    # opacity = ~num_trips_binned*0.1+0.5,
-                   weight = ~num_trips_binned*0.2+0.3) 
+                   # weight = ~num_trips_binned*0.2+0.3
+                   ) 
   }) %>% 
     bindEvent(input$m_marker_click)
 }
